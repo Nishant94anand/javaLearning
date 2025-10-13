ea92c2ea49db4d8a4587b03ebbdcb332809159b0
Nishant Anand <anandnishant@microsoft.com>
2025-10-13 17:38:24 +0530

    JShell Project

---
+++ JShellPractice/src/jShellPracticePackage/FormattedPrinting.java
@@ -0,0 +1,11 @@
+package jShellPracticePackage;
+
+public class FormattedPrinting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%d %d %d", 1, 2, 2*3); // 1 2 6$9 ==> java.io.PrintStream@728938a9
+		System.out.printf("%d %d %d", 1, 2, 2*3).println(); // 1 2 6
+	}
+
+}
---
+++ JShellPractice/src/jShellPracticePackage/MultiplicationTableFormattedPrinting.java
@@ -0,0 +1,12 @@
+package jShellPracticePackage;
+
+public class MultiplicationTableFormattedPrinting {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; ++i) {
+			System.out.printf("5 x %d = %d", i, 5 * i).println();
+		}
+
+	}
+
+}
