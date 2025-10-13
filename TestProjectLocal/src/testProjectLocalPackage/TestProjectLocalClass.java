2a95ce106fd32230dc7464e312884c1c257bd5a2
Nishant Anand <anandnishant@microsoft.com>
2025-10-13 17:35:40 +0530

    Test Project Local Laptop - JRE 17

---
+++ TestProjectLocal/src/testProjectLocalPackage/TestProjectLocalClass.java
@@ -0,0 +1,10 @@
+package testProjectLocalPackage;
+
+public class TestProjectLocalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+	}
+
+}
